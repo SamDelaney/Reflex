c56193f07d450eedd8875aa59bb0c0b2ea103b3a
Sam Delaney <sam@thedelaneys.net>
2020-05-31 21:21:46 -0700

    Updates

---
+++ precache-manifest.f30ec341e0a0806559d5ddec75209229.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a41dda37472ab52d8a4f185ff27c181c",
+    "url": "/LITEr/index.html"
+  },
+  {
+    "revision": "b7884611fed6ef32b01e",
+    "url": "/LITEr/static/css/main.cd22f453.chunk.css"
+  },
+  {
+    "revision": "ef5d1b62d0122bcaa1bc",
+    "url": "/LITEr/static/js/2.5699449e.chunk.js"
+  },
+  {
+    "revision": "be1a5b7d2b271a68932cff2f7d7c74d3",
+    "url": "/LITEr/static/js/2.5699449e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7884611fed6ef32b01e",
+    "url": "/LITEr/static/js/main.cbe809f3.chunk.js"
+  },
+  {
+    "revision": "b146b546b62b1cf50786",
+    "url": "/LITEr/static/js/runtime-main.62789470.js"
+  }
+]);
