d75a70ab1b5696ab3b701feefbab11007ff4c327
Sam Delaney <sam@thedelaneys.net>
2020-06-01 10:01:02 -0700

    improve routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,19 @@ import React from 'react';
 import MainPage from './pages/MainPage';
 import SettingsPage from './pages/Settings';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import LITEAppBar from './components/LITEAppBar';
 
 function AppRouter() {
   return (
     <>
     <LITEAppBar />
-    <Router>
-      <div className='App-header'>
-        <Route path="/" exact component = {MainPage} />
-        <Route path="/settings" exact component = {SettingsPage} />
-      </div>
-    </Router>
+      <HashRouter basename='/'>
+        <div className='App-header'>
+          <Route path="/" exact component = {MainPage} />
+          <Route path="/settings" exact component = {SettingsPage} />
+        </div>
+      </HashRouter>
     </>
   )
 }
