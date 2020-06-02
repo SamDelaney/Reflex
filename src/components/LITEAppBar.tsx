8db7e9c0005be028d9c95945b46ad119428186af
Sam Delaney <sam@thedelaneys.net>
2020-06-02 00:27:03 -0700

    implement routing control

--- src/components/LITEAppBar.tsx
+++ src/components/LITEAppBar.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { withLocalize } from 'react-localize-redux';
-import {AppBar, Toolbar, IconButton, Typography, makeStyles, Theme, createStyles} from '@material-ui/core';
+import { withLocalize, Translate } from 'react-localize-redux';
+import {AppBar, Toolbar, IconButton, Typography, makeStyles, Theme, createStyles, Drawer, List, ListItem, ListItemText} from '@material-ui/core';
 import {Menu, AccountCircle, Settings} from '@material-ui/icons';
+import { Link, NavLink } from 'react-router-dom';
 
 const AppBarStyles = makeStyles((theme: Theme) => 
     createStyles({
+        appBar: {
+            zIndex: theme.zIndex.drawer + 1,
+        },
         title: {
             flexGrow: 1
         }
@@ -13,33 +17,72 @@ const AppBarStyles = makeStyles((theme: Theme) =>
 
 function LITEAppBar() {
     const classes = AppBarStyles();
+
+    const [state, setState] = React.useState({
+        drawerOpen: false
+    });
+
+    const toggleDrawer = (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent,
+        ) => {
+          if (
+            event.type === 'keydown' &&
+            ((event as React.KeyboardEvent).key === 'Tab' ||
+              (event as React.KeyboardEvent).key === 'Shift')
+          ) {
+            return;
+          }
+      
+          setState({ ...state, drawerOpen: open });
+        };
+
     return (
-        <AppBar position="static">
-            <Toolbar >
-                <IconButton edge="start" color="inherit" aria-label="menu">
-                    <Menu />
-                </IconButton>
+        <>
+            <AppBar position="static" className={classes.appBar}>
+                <Toolbar >
+                    <IconButton edge="start" color="inherit" aria-label="menu" onClick={toggleDrawer(!state.drawerOpen)}>
+                        <Menu />
+                    </IconButton>
 
-                <Typography variant="h6" color="inherit" className={classes.title}>
-                    LITEr
-                </Typography>
+                    <Typography variant="h6" color="inherit" className={classes.title}>
+                        LITEr
+                    </Typography>
 
-                <IconButton
-                    aria-label="account"
-                    color="inherit"
-                >
-                    <AccountCircle />
-                </IconButton>
-
-                <IconButton
-                    edge="end"
-                    aria-label="settings"
-                    color="inherit"
-                >
-                    <Settings />
-                </IconButton>
-            </Toolbar >
-        </AppBar>
+                    <IconButton
+                        aria-label="account"
+                        color="inherit"
+                    >
+                        <AccountCircle />
+                    </IconButton>
+                        <IconButton
+                            edge="end"
+                            aria-label="settings"
+                            color="inherit"
+                            component={Link}
+                            to="/settings"
+                        >                            
+                                <Settings />
+                        </IconButton>
+                </Toolbar >
+            </AppBar>
+            <Drawer variant="persistent" open={state.drawerOpen}>
+                <Toolbar />
+                <List>
+                    <ListItem button key="convert" component={ Link } to="/">
+                        <ListItemText primary={<Translate id="menuDrawer.convert" />}/>
+                    </ListItem>
+                    <ListItem button key="styles" component={ Link } to="/styles">
+                        <ListItemText primary={<Translate id="menuDrawer.styles" />}/>
+                    </ListItem>
+                    <ListItem button key="history" component={ Link } to="/history">
+                        <ListItemText primary={<Translate id="menuDrawer.history" />}/>
+                    </ListItem>
+                    
+                    <ListItem button key="settings" component={ Link } to="/settings">
+                        <ListItemText primary={<Translate id="menuDrawer.settings" />}/>
+                    </ListItem>
+                </List>
+            </Drawer>
+        </>
     )
 }
 
---
+++ src/pages/Styles.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function FormatStyles() {
+    return (
+        <div>
+            <h1> Format Styles Placeholder</h1>
+            LITEr is under development. Check back later for more updates!
+        </div>
+    )
+}
+
+export default FormatStyles;
